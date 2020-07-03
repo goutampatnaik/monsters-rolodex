9478ad088f60c22a133e9006c73ab2a40e0cf1ac
goutam patnaik <goutam.nitr@gmail.com>
2020-07-03 09:17:04 +0530

    Updates

---
+++ precache-manifest.d1d34eb58f1805c6ecd3d4aeb8da4189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec197056e0c3514bea950f617f040dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e34fe8a4a7619549861d",
+    "url": "/monsters-rolodex/static/css/main.30172822.chunk.css"
+  },
+  {
+    "revision": "673dfe72c4138960eea7",
+    "url": "/monsters-rolodex/static/js/2.734e0697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.734e0697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34fe8a4a7619549861d",
+    "url": "/monsters-rolodex/static/js/main.2f616b48.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
